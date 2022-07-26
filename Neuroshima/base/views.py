727c96d6cc8168839b8ddeffd8dcefb43dec5821
Dominik <24534yrut@gmail.com>
2022-07-26 22:01:46 +0100

    login page

--- Neuroshima/base/views.py
+++ Neuroshima/base/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 # Create your views here.
 
+def loginPage(request):
+    page = "login"
+    login_error = None
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == "POST":
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            login_error = "Niepoprawny login/i lub hasło!"
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            login_error = "Niepoprawny login/i lub hasło!"
+
+    context = {'page': page, 'login_error': login_error}
+    return render(request, 'base/login_register.html', context)
+
+
 def home(request):
     context = {}
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
+
