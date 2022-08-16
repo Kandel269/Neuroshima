c4162418f32b45d8ea9d48ef1a9e075a2e74f554
Dominik <24534yrut@gmail.com>
2022-08-16 20:37:44 +0100

    score tournaments

--- Neuroshima/base/views.py
+++ Neuroshima/base/views.py
@@ -250,16 +250,44 @@ def tournament_statistics(request,pk):
     duels = Duels.objects.filter(tournament=tournament)
     armies = Armies.objects.all()
     list_score_2D_armies = []
+    armies_list = []
 
-    duels_player_list = []
+    for army in armies:
+        armies_list.append(str(army))
+        army_score_list = []
+        army_score_list.append(army.name)
+        for _ in range(len(armies)):
+            army_score_list.append(0)
+        list_score_2D_armies.append(army_score_list)
 
     for duel in duels:
-        players = []
+        player_1 = ""
+        army_1_index = 0
+        player_2 = ""
+        army_2_index = 0
+
+        lock = 0
+        winner = str(duel.winner)
         for player in duel.users.all():
-             players.append(player)
-        duels_player_list.append(players)
+            if lock == 0:
+                army_1 = str(player.army)
+                army_1_index = armies_list.index(army_1)
+                player_1 = str(player.user.username)
+                lock += 1
+            army_2 = str(player.army)
+            army_2_index = armies_list.index(army_2)
+            player_2 = str(player.user.username)
+
+        if winner == player_1:
+            list_score_2D_armies[army_1_index][army_2_index + 1] += 1
+        elif winner == player_2:
+            list_score_2D_armies[army_2_index][army_1_index + 1] += 1
+
+    list_score_win_ratio = []
+
+    for army_score in len(range(list_score_2D_armies)):
+        for score in len(range(army_score)):
 
 
-
-    context = {'tournament':tournament,'armies':armies}
+    context = {'tournament':tournament,'armies':armies,'list_score_2D_armies':list_score_2D_armies}
     return render(request, 'tournament/statistics.html', context)
