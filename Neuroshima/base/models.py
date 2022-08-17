5ebd4fa1927a97b3b3c751e0bc56d44dafa212ee
Dominik <24534yrut@gmail.com>
2022-08-17 20:05:03 +0100

    now good

--- Neuroshima/base/models.py
+++ Neuroshima/base/models.py
@@ -35,14 +35,13 @@ class Tournaments(models.Model):
 #     lose_count = models.IntegerField()
 
 
-class Duels(models.Model):
-    tournament = models.ForeignKey(Tournaments, on_delete= models.SET_NULL, null = True, blank = True)
-    users = models.ManyToManyField(User, through = 'DuelUser', blank = True)
-    winner = models.CharField(max_length = 250, blank = True)
-    hp_gap = models.IntegerField()
-
 class DuelUser(models.Model):
     user = models.ForeignKey(User, on_delete= models.SET_NULL, null = True, blank = True)
     army = models.ForeignKey(Armies, on_delete= models.SET_NULL, null = True)
     hp = models.IntegerField()
-    duels = models.ForeignKey(Duels, on_delete = models.CASCADE)
+
+class Duels(models.Model):
+    tournament = models.ForeignKey(Tournaments, on_delete= models.SET_NULL, null = True, blank = True)
+    users = models.ManyToManyField(DuelUser, null = True, blank = True)
+    winner = models.CharField(max_length = 250, blank = True)
+    hp_gap = models.IntegerField()
