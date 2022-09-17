f1aedfa1f6b619ffda1ad66f0f1c6c0d0cd21e35
Dominik <24534yrut@gmail.com>
2022-09-17 12:07:26 +0100

    csv reader/writer v2

--- Neuroshima/base/csv_reader.py
+++ Neuroshima/base/csv_reader.py
@@ -4,7 +4,7 @@ def read_csv(filename):
     """ Wczytywanie i wyswietalnie danych z pliku CSV """
     try:
         with open(filename, newline='') as csv_file:
-            csv_reader = csv.reader(csv_file)
+            csv_reader = csv.DictReader(csv_file)
             for record in csv_reader:
                 print(record)
     except(IOError, OSError) as file_read_error:
--- Neuroshima/base/csv_writer.py
+++ Neuroshima/base/csv_writer.py
@@ -9,16 +9,17 @@ def write_csv(filename, header, data):
      """
     try:
         with open(filename, "w") as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(header)
-            for record in data:
-                csv_writer.writerow(record)
+            csv_writer = csv.DictWriter(csv_file, fieldnames=header)
+            csv_writer.writeheader()
+            csv_writer.writerows(data)
     except (IOError, OSError) as csv_file_error:
         print(f"Nie można zapisać danych w pliku csv. Wyjątek {csv_file_error}")
 
 if __name__ == '__main__':
     header = ['name','age','gender']
-    data = [['Dominik','32','M'], ['Ania','23','K'], ['Zbycho','44','M']]
+    data = [{'name':'Dominik','age':'32','gender':'M'},
+            {'name':'Ania','age':'23','gender':'K'},
+          {'name':'Zbycho','age':'44','gender':'M'}]
     filename = 'sample_output.csv'
     write_csv(filename,header,data)
 
