04f4bcd9e296c6e37b549694686a6497aa569852
Dominik <24534yrut@gmail.com>
2022-09-18 13:23:13 +0100

    plot

--- Neuroshima/base/pdf_demo.py
+++ Neuroshima/base/pdf_demo.py
@@ -3,12 +3,12 @@ import os
 GTK_FOLDER = r'C:\Program Files\GTK3-Runtime Win64\bin'
 os.environ['PATH'] = GTK_FOLDER + os.pathsep + os.environ.get('PATH', '')
 
-from weasyprint import HTML
+from weasyprint import HTML, CSS
 
 def generate_pdf(url, pdf_file):
     """ Generowanie doumentu PDF na podstawie stronyz podanego adresu URL """
-    print("Generowanie dokumentu PDF...")
-    HTML(url).write_pdf(pdf_file)
+    css = CSS(string='body{ font-size: 8px; }')
+    HTML(url).write_pdf(pdf_file, stylesheets=[css])
 
 if __name__ == '__main__':
     url = 'http://text.npr.org'
