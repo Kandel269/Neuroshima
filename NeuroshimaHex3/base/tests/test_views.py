2e5dba6669dbdc1417291d5c980518fb88023f78
Dominik <24534yrut@gmail.com>
2022-09-25 16:26:29 +0100

    tests vv2

--- NeuroshimaHex3/base/tests/test_views.py
+++ NeuroshimaHex3/base/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
+from ..models import Tournaments
 
 
 class TestHomeView(TestCase):
@@ -10,7 +11,7 @@ class TestHomeView(TestCase):
         response = self.client.get('')
         self.assertEquals(response.status_code, 200)
 
-class TestCreateTournament(TestCase):
+class TestCreateTournamentView(TestCase):
     def setUp(self):
         self.test_user = User.objects.create_user(username= "Noob", password="Noobpassword")
         self.test_user.save()
@@ -24,3 +25,31 @@ class TestCreateTournament(TestCase):
         login = self.client.login(username= "Noob", password="Noobpassword")
         response = self.client.get('/twoj-profil/stworz-turniej/')
         self.assertEquals(response.status_code, 200)
+
+class TestDeleteTournamentView(TestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(username="Noob",password="Noobpassword")
+        self.test_user.save()
+        self.wrong_user = User.objects.create_user(username="Newbie",password="Newbiepassword")
+        self.wrong_user.save()
+
+        self.client = Client()
+        self.obj = Tournaments(host = self.test_user, name = "NoobTournament")
+        self.obj.save()
+
+    def test_wrong_user_delete_tournament(self):
+        login = self.client.login(username="Newbie",password="Newbiepassword")
+        test_id = self.obj.id
+        response = self.client.get(f'/turniej/{test_id}/usun-turniej/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_user_delete_tournament(self):
+        login = self.client.login(username="Noob",password="Noobpassword")
+        test_id = self.obj.id
+        response = self.client.get(f'/turniej/{test_id}/usun-turniej/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_anonymus_user_delete_tournament(self):
+        test_id = self.obj.id
+        response = self.client.get(f'/turniej/{test_id}/usun-turniej/')
+        self.assertEquals(response.status_code, 302)
